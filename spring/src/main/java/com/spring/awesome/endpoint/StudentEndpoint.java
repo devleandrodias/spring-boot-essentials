88f3026f0843d1e76bb405dd49a81165aaefad68
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-01-04 16:10:08 -0300

    POST do Student

--- spring/src/main/java/com/spring/awesome/endpoint/StudentEndpoint.java
+++ spring/src/main/java/com/spring/awesome/endpoint/StudentEndpoint.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,6 +50,15 @@ public class StudentEndpoint {
       return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
     else
       return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
+  }
+
+  // POST - Incluir
+  @RequestMapping(method = RequestMethod.POST)
+  public ResponseEntity<?> save(@RequestBody Student student) {
+
+    Student.studentList.add(student);
+
+    return new ResponseEntity<>(student, HttpStatus.OK);
   }
 }
 
