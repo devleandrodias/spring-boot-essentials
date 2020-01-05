117efe75d09d8fe53b688e6e4c660e97521d6fd1
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-01-05 12:49:47 -0300

    adicionando segurança a API

--- spring/src/main/java/com/spring/awesome/endpoint/StudentEndpoint.java
+++ spring/src/main/java/com/spring/awesome/endpoint/StudentEndpoint.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -100,6 +101,7 @@ public class StudentEndpoint {
   // Idempotent (Se você fizer várias vezes a mesma requisição mesmo resultado)
   // @RequestMapping(method = RequestMethod.DELETE)
   @DeleteMapping(path = "/{id}") // Subtitui o de cima
+  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<?> delete(@PathVariable("id") Long id) {
 
     verifyIfStudentsExists(id);
